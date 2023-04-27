d4ba0b9c3355a009e5ba576bc7f5d514f3ac83f9
dasabhisek96 <abhisek.das.96@gmail.com>
2023-04-27 16:28:28 +0530

    add log

--- src/models/db.js
+++ src/models/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports = async () => {
     try {
+        console.log(process.env.MONGOURL);
         await mongoose.connect(process.env.MONGOURL, {
             useNewUrlParser: true,
             useUnifiedTopology: true
